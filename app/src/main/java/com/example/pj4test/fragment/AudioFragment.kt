f73697e9c0cbcec8fb8bdced64c078ff00833613
Jaehong Cho <83823618+JaehongCS20@users.noreply.github.com>
2023-06-05 15:56:16 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pj4test/fragment/AudioFragment.kt
@@ -0,0 +1,70 @@
+package com.example.pj4test.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.pj4test.ProjectConfiguration
+import com.example.pj4test.audioInference.GooseClassifier
+import com.example.pj4test.databinding.FragmentAudioBinding
+
+class AudioFragment: Fragment(), GooseClassifier.DetectorListener {
+    private val TAG = "AudioFragment"
+
+    private var _fragmentAudioBinding: FragmentAudioBinding? = null
+
+    private val fragmentAudioBinding
+        get() = _fragmentAudioBinding!!
+
+    // classifiers
+    lateinit var gooseClassifier: GooseClassifier
+
+    // views
+    lateinit var snapView: TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _fragmentAudioBinding = FragmentAudioBinding.inflate(inflater, container, false)
+
+        return fragmentAudioBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        snapView = fragmentAudioBinding.SnapView
+
+        gooseClassifier = GooseClassifier()
+        gooseClassifier.initialize(requireContext())
+        gooseClassifier.setDetectorListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        gooseClassifier.stopInferencing()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        gooseClassifier.startInferencing()
+    }
+
+    override fun onResults(score: Float) {
+        activity?.runOnUiThread {
+            if (score > GooseClassifier.THRESHOLD) {
+                snapView.text = "GOOSE"
+                snapView.setBackgroundColor(ProjectConfiguration.activeBackgroundColor)
+                snapView.setTextColor(ProjectConfiguration.activeTextColor)
+            } else {
+                snapView.text = "NO GOOSE"
+                snapView.setBackgroundColor(ProjectConfiguration.idleBackgroundColor)
+                snapView.setTextColor(ProjectConfiguration.idleTextColor)
+            }
+        }
+    }
+}
